c29c88023301a2306285281d7e47542092c801ba
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2020-10-12 18:43:00 +0530

    Update Snake.java

--- Snake Game/src/Snake.java
+++ Snake Game/src/Snake.java
@@ -1,20 +1,15 @@
 package snake;
 
 import javax.swing.*;
-// we cannot add window ke andar window
-// we can add window ke andar container;
 
 public class Snake extends JFrame {
 	Snake(){
 		add(new Board());
 		pack();
-		// we use setLocationRelative to set frame at the centre;
-		// in order to set the frame at other locations then use setLocation method;
-		
-		// to set a title of the game, we use setTitle() or else we can call super("title of the game")
+		 
 		setLocationRelativeTo(null);  
 		setTitle("Snake Game");
-		setResizable(false);  // by setting setResizable false size of the window won't change;
+		setResizable(false);  
 	}
 	
 	public static void main(String args[]) {
